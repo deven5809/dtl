44d41ec7fa88ffc8a8819e99b3470cf639a53780
dan <deven5809@gmail.com>
2025-07-17 16:13:13 -0500

    Added build variable to use g++13

--- build.sh
+++ build.sh
@@ -8,7 +8,7 @@ cd out
 
 # Run CMake to generate a build system. We also want it to output a compile
 # commands json file for VS Code to parse.
-cmake ../ -DCMAKE_EXPORT_COMPILE_COMMANDS=ON
+cmake ../ -DCMAKE_EXPORT_COMPILE_COMMANDS=ON -DCMAKE_CXX_COMPILER="/usr/bin/g++-13"
 
 # Perform the build.
 cmake --build .
